6d562403fb8a75b20db3c4628a2dd65034780ab8
Ester Lina Maudya L <42627976+esterlinamaudya@users.noreply.github.com>
2018-09-13 08:31:36 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,40 @@
 <?php
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
+$batas = 5;
+function rekursif($i = 0){
+  global $batas;
+  echo "*";
+  $i++;
+  
+  if($i<$batas){
     rekursif($i);
+}else{
+    echo "<br>";
+    $batas--;
+    if($batas!=0){
+      rekursif();
+    }
+  }
 }
-rekursif(5);
+rekursif();
+
+echo "<br>";
+
+$batass = 1;
+function rekursiff($j=1){
+  global $batass;
+	echo "$j";
+	$j++;
+
+	if($j<=$batass){
+	rekursiff($j);
+	}else{
+	echo "<br>";
+	$batass++;
+	if($batass<=5){
+		rekursiff();
+	}
+}
+}
+rekursiff();
 ?>
